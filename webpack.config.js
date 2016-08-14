d989d18e5ce8fdec339b455efcdd877fc8bf3b1c
Andre Walker-Upshur <Andre Walker-Upshur>
2016-08-14 15:14:53 -0400

    Support foundation settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   entry: [
@@ -30,7 +31,6 @@ module.exports = {
       Clock: 'app/components/Clock.jsx',
       CountdownForm: 'app/components/CountdownForm.jsx',
       Controls: 'app/components/Controls.jsx',
-      TimerControls: 'app/components/TimerControls.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
@@ -44,6 +44,11 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
       }
+    ]
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
   devtool: 'cheap-module-eval-source-map'
